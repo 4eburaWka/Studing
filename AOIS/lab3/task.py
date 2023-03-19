28ea3dfe66bbe84da3d2cf836cf7ff9db7362f1e
4eburaWka <prikpasa14@gmail.com>
2023-03-19 20:45:50 +0300

    upadte

--- AOIS/lab3/task.py
+++ AOIS/lab3/task.py
@@ -1,56 +1,32 @@
 import numpy as np
 
-# Задаем параметры нейросети
-input_size = 8 # размер входных данных
-output_size = 12 # размер выходных данных
-hidden_size = 16 # размер скрытого слоя
-learning_rate = 0.01 # коэффициент скорости обучения
+class Network:
+    def __init__(self, n, m) -> None:
+        self.n, self.m = n, m
 
-# Инициализируем веса нейронов
-w1 = np.random.normal(size=(input_size, hidden_size))
-w2 = np.random.normal(size=(hidden_size, output_size))
+    def predict(self):
+        pass
 
-# Определяем функцию активации нейронов
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    def train(self, ref_img):
+        self.W = np.array(ref_img) / 2
+        self.T = np.full(self.m, self.n / 2)
+        Y1 = np.zeros(3)
+        for i in range(self.m):
+            Y1[i] = np.sum([ref_img[i][j] *  self.W[i][j] for j in range(self.n)]) - self.T[i]
+        
+        e = np.random.random() / 1 / self.m
+        self.V = np.full((self.m, self.m), -e) # заполнить матрицу весов для НС Хопфилда е
+        for i in range(self.m):
+            self.V[i][i] = 1 # заполнение 1 по диагонали
+        
+        print(Y1)
 
-# Определяем функцию для вычисления производной функции активации
-def sigmoid_prime(x):
-    return sigmoid(x) * (1 - sigmoid(x))
 
-# Определяем функцию ошибки
-def mse_loss(y_true, y_pred):
-    return ((y_true - y_pred) ** 2).mean()
+reference_image = [
+    [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1]
+]
 
-# Определяем функцию для обучения нейросети
-def train(X, y, w1, w2, learning_rate):
-    # Прямое распространение
-    hidden_layer = sigmoid(np.dot(X, w1))
-    output_layer = sigmoid(np.dot(hidden_layer, w2))
-
-    # Вычисление функции ошибки
-    loss = mse_loss(y, output_layer)
-
-    # Вычисление градиентов
-    output_error = (y - output_layer) * sigmoid_prime(output_layer)
-    hidden_error = np.dot(output_error, w2.T) * sigmoid_prime(hidden_layer)
-    output_gradient = np.dot(hidden_layer.T, output_error)
-    hidden_gradient = np.dot(X.T, hidden_error)
-
-    # Обновление весов
-    w2 += learning_rate * output_gradient
-    w1 += learning_rate * hidden_gradient
-
-    return w1, w2, loss
-
-# Обучаем нейросеть
-for i in range(1000):
-    # Генерируем случайные данные
-    input_data = np.array([[0,1,1,0,1,1,0,0]] * 10)
-    target_data = np.array([[1,0,1,0,0,0,1,1,0,1,1,0]] * 10)
-
-    # Обновляем веса нейросети
-    w1, w2, loss = train(input_data, target_data, w1, w2, learning_rate)
-
-    # Выводим текущее значение функции ошибки
-    print("Step %d, loss = %f" % (i, loss))
+NN = Network()
+NN.train(reference_image)
---
+++ KMZI/LAB2/header.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+void subs(string text, int i, int j, string key){
+    char tabl[i][j+1]; int x;
+    for (x = 0; x < i; x++)
+        tabl[x][j] = '\0';
+    while (1){ // удалить пробелы из строки
+        x = text.find(' ');
+        if (x == -1)
+            break;
+        text.erase(x, 1);
+    }
+
+    for (x = 0; x < text.size(); x++){
+        tabl[x % i][x / i] = text[x];
+    }
+    
+    string str;
+    for (int o = 0; o < i; o++){
+        for (int t = 0; t < j; t++){
+            str += tabl[o][t];
+
+        }
+    }
+    for (x = 4; x < str.size(); x += 4)
+        str.insert(x++, " ");
+    cout << str;
+}
+
+int main(){
+    subs("Thebishoplookedliketheh6squarethencapturedthepawn", 5, 10, " ");
+}
