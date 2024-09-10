38d0c7e2d12d19cf7ea3edb751d1440fdee5b5ed
4eburaWka <prikpasa14@gmail.com>
2024-09-10 08:32:42 +0300

    delete old labs

--- GIIS/Lab2/main/address_book/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import ContactModel
-
-
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactModel
-        fields = ['id', 'first_name', 'last_name', 'surname', 'phone', 'address']
